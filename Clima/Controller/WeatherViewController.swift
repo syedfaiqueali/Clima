1778b01980d169834e6962fa1fce0f4f2dd2a1b6
Faiq Ali <faiqueali017@gmail.com>
2020-12-05 22:00:43 +0500

    TextField delegates methods added

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,51 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
+    //MARK:- IBOutlet
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
     }
 
-
+    //MARK:- IBAction
+    @IBAction func searchPressed(_ sender: UIButton) {
+        //to dismiss the keyboard
+        searchTextField.endEditing(true)
+        
+        print(searchTextField.text!)
+    }
+    
+    //MARK:- SearchTextField delegate methods
+    //When user press 'Go' button from keyboard. So it act as a return keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //to dismiss the keyboard
+        searchTextField.endEditing(true)
+        
+        print(searchTextField.text!)
+        return true
+    }
+    
+    //To check if the user has typed something or not and prompt the user if the search text field is empty
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != ""{
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    //When user stops editing so clears previous text from search text field
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
 }
 
