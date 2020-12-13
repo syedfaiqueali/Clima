19f146acdf45de5f03c88439b19c435a231f2c28
Faiq Ali <faiqueali017@gmail.com>
2020-12-13 19:39:16 +0500

    Refactor getConditionName() to a computed property named 'conditionName'

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -69,31 +69,21 @@ struct WeatherManager {
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
             let id = decodedData.weather[0].id
-            print(getConditionName(weatherId: id))
-            //print(decodedData.weather[0].description)
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            //Instantiating weather struct
+            let weather = WeatherModel(
+                conditionId: id,
+                cityName: name,
+                temperature: temp)
+            //calling func to get type of weather
+            print(weather.conditionName)
+            
         } catch {
             print(error)
         }
     }
     
-    func getConditionName(weatherId: Int) -> String {
-        switch weatherId {
-        case 200...232:
-            return "cloud.bolt"
-        case 300...321:
-            return "cloud.drizzle"
-        case 500...531:
-            return "cloud.rain"
-        case 600...622:
-            return "cloud.snow"
-        case 701...781:
-            return "cloud.fog"
-        case 800:
-            return "sun.max"
-        case 801...804:
-            return "cloud.bolt"
-        default:
-            return "cloud"
-        }
-    }
+    
 }
---
+++ Clima/Model/WeatherModel.swift
@@ -0,0 +1,38 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Faiq on 13/12/2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherModel {
+    let conditionId: Int
+    let cityName: String
+    let temperature: Double
+    
+    //computed property
+    var conditionName: String {
+        switch conditionId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud.bolt"
+        default:
+            return "cloud"
+        }
+    }
+    
+}
